32148b390f612f206f2c90f627e6c79c38fc8259
ChoSangok <chod1510@naver.com>
2023-07-11 22:17:17 +0900

    feat/Banner

---
+++ src/components/Banner.js
@@ -0,0 +1,24 @@
+import React from "react";
+const array = [1, 2, 3, 4, 5];
+
+const Banner = () => {
+  return (
+    <div className="flex flex-row w-[1128px] overflow-hidden mb-20">
+      {array.map((item, idx) => {
+        return (
+          <div>
+            <div
+              key={idx}
+              className="flex justify-center items-center rounded-xl bg-black text-white w-[264px] h-[210px] mr-3"
+            >
+              Banner
+            </div>
+            <p></p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,22 @@
 import React from "react";
+import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 
 const Main = () => {
   return (
     <div className="flex flex-col w-[1280px] h-screen">
-      <header className="grow">
+      <header className="h-[80px]">
         <Nav />
       </header>
+      <main className="grow">
+        <section className="m-20">
+          <p className="font-bold text-2xl mb-5">상품 리스트</p>
+          <Banner />
+          <p className="font-bold text-2xl mb-5">북마크 리스트</p>
+          <Banner />
+        </section>
+      </main>
       <footer className=" bottom-0">
         <Footer />
       </footer>
