4a080c52eb37ab2332e1d1945eae9fe52256d67a
ChoSangok <chod1510@naver.com>
2023-07-11 20:50:07 +0900

    feat/footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-white w-[1280px] h-[58px] text-center drop-shadow-2xl">
+      <p className="text-gray-400">개인정보 처리방침 | 이용약관</p>
+      <p className="text-gray-400">All rights reserved @ Codestates</p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 
 const Main = () => {
   return (
-    <div>
-      <Nav />
+    <div className="flex flex-col w-[1280px] h-screen">
+      <header className="grow">
+        <Nav />
+      </header>
+      <footer className=" bottom-0">
+        <Footer />
+      </footer>
     </div>
   );
 };
