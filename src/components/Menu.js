52e9fa19715c07147579958f8cf71b468adfcbe7
ChoSangok <chod1510@naver.com>
2023-07-12 10:45:01 +0900

    feat/page전환

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,22 +1,36 @@
 import React from "react";
 import { AiFillGift, AiOutlineStar } from "react-icons/ai";
+import { BsFillTriangleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <div className="flex flex-col p-2  justify-center items-left bg-white drop-shadow-md w-[200px] h-[150px] rounded-xl">
-      <div className="flex items-center">
-        <p className="text-xl p-1">OOO님, 안녕하세요!</p>
+    <div
+      className="
+      flex
+      flex-col
+      absolute
+      top-4
+      right-0
+      mr-[-20px]
+       rounded-2xl
+       shadow-lg
+    "
+    >
+      <BsFillTriangleFill className="text-gray-100 text-2xl ml-40 mb-[-5px]"></BsFillTriangleFill>
+      <div className=" flex flex-col justify-center items-left bg-gray-100   w-[200px] h-[150px] rounded-xl">
+        <div className="flex items-center border-b">
+          <p className="text-xl p-2 ">OOO님, 안녕하세요!</p>
+        </div>
+        <Link to="/Product" className="flex items-center p-2 border-b">
+          <AiFillGift />
+          <p className="text-xl ">상품리스트 페이지</p>
+        </Link>
+        <Link to="/BookMark" className="flex items-center p-2  ">
+          <AiOutlineStar />
+          <p className="text-xl ">북마크 페이지</p>
+        </Link>
       </div>
-
-      <Link to="/Product" className="flex items-center">
-        <AiFillGift />
-        <p className="text-xl p-1">상품리스트 페이지</p>
-      </Link>
-      <Link to="/BookMark" className="flex items-center">
-        <AiOutlineStar />
-        <p className="text-xl p-1">북마크 페이지</p>
-      </Link>
     </div>
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaHamburger } from "react-icons/fa";
 import Menu from "./Menu";
 
 const Nav = () => {
+  const [menu, setMenu] = useState(false);
+
+  const menuOnOf = () => {
+    if (!menu) {
+      setMenu(true);
+    } else {
+      setMenu(false);
+    }
+  };
+
   return (
     <div className="flex  flex-row items-center fixed  bg-white w-[1280px] h-[80px] shadow-lg">
       <div className="flex flex-col items-end place-content-center w-[55px] h-[30px] ml-10 m-2">
@@ -12,8 +22,15 @@ const Nav = () => {
           <div className=" bg-indigo-950 w-[35px] h-[10px] rounded-md ml-1"></div>
         </div>
       </div>
-      <p className=" text-4xl font-bold m-3">COZ Shopping</p>
-      <FaHamburger className="absolute right-10" />
+      <p className=" text-4xl font-bold m-3 ">COZ Shopping</p>
+      <button className="flex  absolute right-10" onClick={menuOnOf}>
+        <FaHamburger />
+      </button>
+      {menu && (
+        <div className="flex  absolute right-10">
+          <Menu />
+        </div>
+      )}
     </div>
   );
 };
