48c92c093805d93582528ede176554b29ec5f423
ChoSangok <chod1510@naver.com>
2023-07-13 17:28:43 +0900

    feat/Banner기능구현

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,17 @@
 import React from "react";
+import {
+  BRANDNAME,
+  BRANDIMAGE,
+  BRANDFOLLOWER,
+  DISCOUNT,
+  PRICE,
+  TITLE,
+  IMAGE,
+  SUBTITLE,
+} from "../constants";
 
 const Banner = ({ products }) => {
+  console.log(products[0]);
   return (
     <div className="flex flex-row w-[1128px] overflow-hidden mb-5">
       {products.length !== 0 &&
@@ -13,17 +24,35 @@ const Banner = ({ products }) => {
               bg-black text-white w-[264px] h-[210px] mr-3"
               >
                 <img
-                  className="h-full rounded-2xl w-[264px] "
-                  src={item.brand_image_url}
-                  alt={item.BRANDNAME}
+                  className="h-full rounded-2xl w-[264px]"
+                  src={item ? item[BRANDIMAGE] || item[IMAGE] : ""}
+                  alt={item ? item[BRANDNAME] || item[TITLE] : ""}
                 />
               </div>
               <div className="flex flex-col ">
-                <div className="flex ">
-                  <p>{item.brand_name}</p>
-                  <p>관심 고객수</p>
+                <div className="flex">
+                  <p>
+                    {item
+                      ? item.type === "Category"
+                        ? "#" + item[TITLE]
+                        : item[TITLE] || item[BRANDNAME]
+                      : ""}
+                  </p>
+                  <p> </p>
                 </div>
-                <p className="">{item.follower}</p>
+                {item.type === "Category" ? (
+                  ""
+                ) : (
+                  <p className="">
+                    {item
+                      ? item.type === "Exhibition"
+                        ? item[SUBTITLE]
+                        : item[PRICE]
+                        ? Number(item[PRICE]).toLocaleString() + "원"
+                        : Number(item[BRANDFOLLOWER]).toLocaleString()
+                      : ""}
+                  </p>
+                )}
               </div>
             </div>
           );
---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const BRANDNAME = "brand_name";
+export const BRANDIMAGE = "brand_image_url";
+export const BRANDFOLLOWER = "follower";
+export const DISCOUNT = "discountPercentage";
+export const PRICE = "price";
+export const TITLE = "title";
+export const IMAGE = "image_url";
+export const SUBTITLE = "sub_title";
